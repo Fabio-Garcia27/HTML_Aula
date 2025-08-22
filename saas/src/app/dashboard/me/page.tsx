229deb00ad6bf2f8c152a309c6389f837112b98b
Fábio Garcia <fabio@unipar.br>
2025-08-22 17:23:10 -0300

    Versão 1.0.0.2

--- saas/src/app/dashboard/me/page.tsx
+++ saas/src/app/dashboard/me/page.tsx
@@ -7,7 +7,7 @@ export default function Me() {
         <p
           className="w-fit h-9 rounded-md flex items-center font-semibold text-white"
         >
-          http://minha_url.com/creator/fulano-dev
+          http://minha_url.com/creator/fhg-dev
         </p>
       </section>
 
