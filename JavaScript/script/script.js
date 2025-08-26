522efa6c0615cbe3da426b1ba01189072bcebf60
Fábio Garcia <fabio@unipar.br>
2025-08-26 14:25:19 -0300

    Versão 1.0.0.1

---
+++ JavaScript/script/script.js
@@ -0,0 +1,8 @@
+let nome = "Fábio Garcia";
+console.log("Olá, " + nome + "!");
+
+let idade = 53;
+console.log("Você tem " + idade + " anos.");
+
+let eMaiorDeIdade = idade >= 18;
+console.log("É maior de idade? " + eMaiorDeIdade);
