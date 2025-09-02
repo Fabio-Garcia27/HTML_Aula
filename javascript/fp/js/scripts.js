aee6b44cac190ab21bd0465718451f0f15089594
Fábio Garcia <fabio@unipar.br>
2025-09-02 14:18:26 -0300

    Versão 1.0.0.4

--- javascript/fp/js/scripts.js
+++ javascript/fp/js/scripts.js
@@ -4,6 +4,7 @@ let container = document.querySelector('.container')
 let items = container.querySelectorAll('.list .item')
 let indicator = document.querySelector('.indicators')
 let dots = indicator.querySelectorAll('ul li')
+let list = container.querySelector('.list')
 
 let active = 0
 let firstPosition = 0
@@ -17,16 +18,20 @@ function setSlider() {
     dotsOld.classList.remove('active')
     dots[active].classList.add('active')
 
-    indicator.querySelector('.number').innerHTML = '0' + active + 1    
+    indicator.querySelector('.number').innerHTML = '0' + (active + 1)
 }
 
 nextButton.onclick = () => {
+    list.style.setProperty('--calculation', 1)
+
     active = active + 1 > lastPosition ? 0 : active + 1
     setSlider()
     items[active].classList.add('active')
 }
 
 prevButton.onclick = () => {
+    list.style.setProperty('--calculation', -1)
+
     active = active - 1 < firstPosition ? lastPosition : active - 1
     setSlider()
     items[active].classList.add('active')
