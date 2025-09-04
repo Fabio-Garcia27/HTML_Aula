895ae4af71545e512572afed52017fbc266a23af
Fábio Garcia <fabio@unipar.br>
2025-09-04 16:57:10 -0300

    Versão 1.0.0.3

--- React Previsão/react-previsao/src/App.jsx
+++ React Previsão/react-previsao/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   }
 
   return (
-    <div>
+    <div className='container'>
       <h1>Previsão do Tempo</h1>
       <input ref={inputRef} type='text' placeholder='Digite o nome da cidade' />
       <button onClick={searchCity}>Buscar</button>
