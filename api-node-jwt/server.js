895390d403ffa84bd3ea8443a04433ebfe7bbd41
Fábio Garcia <fabio@unipar.br>
2025-09-08 19:29:49 -0300

    Versão 1.0.0.2

--- api-node-jwt/server.js
+++ api-node-jwt/server.js
@@ -1,6 +1,13 @@
-import express from 'express';
+import express from 'express'
+import publicRoutes from './routes/public.js'
+import privateRoutes from './routes/private.js'
+import auth from './middlewares/auth.js'
 
-const app = express();
+const app = express()
+app.use(express.json())
+
+app.use('/', publicRoutes)
+app.use('/', auth, privateRoutes)
 
 
 app.listen(3000, () => {
