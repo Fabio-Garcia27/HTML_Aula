13ff90827a63510b08dc3e7ed4934529a878d685
Fábio Garcia <fabio@unipar.br>
2025-09-11 11:00:50 -0300

    Versão 1.0.0.2

--- Node/server.js
+++ Node/server.js
@@ -1,8 +1,34 @@
 import express from "express"
 
 const app = express()
+app.use(express.json())
 
+const users = [{
+  name: "Fábio Garcia",
+  age: 53
+}]
+
+app.get('/usuarios', (req, res) => {
+  res.send(users)
+})
+
+app.post('/usuarios', (req, res) => {
+
+  users.push(req.body)
+
+  res.send(users)
+})
+
+app.delete('/usuarios/:id', (req, res) => {
+
+  const id = req.params.id
+
+  users.splice(id, 1)
+
+  res.send("Usuário deletado com sucesso")
+})
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
-});
+})
+
