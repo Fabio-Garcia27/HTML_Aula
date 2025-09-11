4afc4f8d7fd1c55461ca7d742304c0192d0bb6f6
Fábio Garcia <fabio@unipar.br>
2025-09-11 13:43:22 -0300

    Versão 1.0.0.1

--- Live 35/backend/server.js
+++ Live 35/backend/server.js
@@ -3,6 +3,24 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 
+const users = [{
+  name: "Fábio Garcia",
+  age: 53
+}]
+
+app.get('/usuarios', (req, res) => {
+  res.json(users)
+})
+
+app.post('/usuarios', (req, res) => {
+
+  const newUser = req.body
+
+  users.push(newUser)
+
+  res.status(201).json(newUser)
+})
+
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
 })
