6429b8953f60451e8b228c2cd076d3ce9e338cc6
Fábio Garcia <fabio@unipar.br>
2025-09-15 13:38:03 -0300

    Versão 1.0.0.2

--- javascript/Aula5/script/script.js
+++ javascript/Aula5/script/script.js
@@ -1,7 +1,11 @@
 const valorReal = document.querySelector("input")
 
 function converter() {
+
+    document.querySelector("h1").style.color = "red"
+
     const valorConvertido = valorReal.value / 5
 
     alert(`O valor em dólar é ${valorConvertido} dólares`)
 }
+
