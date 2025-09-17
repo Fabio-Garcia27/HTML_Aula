c02bc7137f182e6bf482e60da56e1e9fb1190a53
Fábio Garcia <fabio@unipar.br>
2025-09-17 19:05:01 -0300

    Versão 1.0.0.2

--- curso-react/react1/src/App.jsx
+++ curso-react/react1/src/App.jsx
@@ -1,13 +1,20 @@
-//import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import Contato from './pages/Contato'
+import './index.css'
 
-import './App.css'
 
 function App() {
-  //const [count, setCount] = useState([])
-
   return (
     <div>
-      <h1>Curso React</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/sobre' element={<Sobre/>} />
+          <Route path='/contato' element={<Contato/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
