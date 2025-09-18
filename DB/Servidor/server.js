72ce71b4a0e2a711931b4da67676f51bbb7ce42b
Fábio Garcia <fabio@unipar.br>
2025-09-18 09:55:31 -0300

    Versão 1.0.0.2

--- DB/Servidor/server.js
+++ DB/Servidor/server.js
@@ -1,1 +1,31 @@
-const express = require('express')
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+let usuarios = []
+
+app.post('/usuarios', (req, res) => {
+    const { nome, email, fone } = req.body
+
+    if (!nome || !email || !fone) {
+    return res.status(400).json({ erro: 'Nome, email e fone são obrigatórios' });
+
+    // Aqui você poderia salvar no banco de dados
+    const novoUsuario = { id: Date.now(), nome, email };
+
+    return res.status(201).json({ mensagem: 'Usuário criado com sucesso', usuario: novoUsuario });
+}
+
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000')
+})
+
+
+
+
+
