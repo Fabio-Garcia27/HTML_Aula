a3de0cdf5be9b3192044b226ba0ea37d8597d8fc
Fábio Garcia <fabio@unipar.br>
2025-12-16 15:47:35 -0300

    Versão 1.0.0.1

--- sites/aulas-js/js/script.js
+++ sites/aulas-js/js/script.js
@@ -1,1 +1,19 @@
-document.getElementById("texto").innerHTML="Meu Primeito texto <b>JS</b>!";
+//Saídas
+// document.getElementById("texto").innerHTML="Meu Primeito texto <b>JS</b>!";
+
+// document.write("Oi escrito com document.write!");
+
+// alert(10+5);
+
+// console.log('Isso é console.log');
+/*
+let = a,b,c;
+a = 5;
+b = 6;
+c = 7;
+*/
+/*
+const a = "Fábio";
+console.log(a);
+*/
+
