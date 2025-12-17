86bd2911731542de7a99579d45b1071bd4e86401
Fábio Garcia <fabio@unipar.br>
2025-12-17 14:53:03 -0300

    Versão 1.0.0.1

--- sites/aulas-js/js/script.js
+++ sites/aulas-js/js/script.js
@@ -17,3 +17,23 @@ const a = "Fábio";
 console.log(a);
 */
 
+//Funçõa
+
+function soma(valor1, valor2) {
+    return (
+        valor1 + valor2
+    )
+}
+
+document.getElementById("texto").innerHTML = soma(10, 10);
+
+
+function realParaDolar (real, cotacaoDolar) {
+    return (
+        real * cotacaoDolar
+    )
+}
+
+const total = realParaDolar(10, 5.08);
+
+alert(total);
