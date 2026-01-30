62a25d3cc7b81a8b7c982765cbc23b83b391e77a
Fábio Garcia <fabio@unipar.br>
2026-01-30 16:18:00 -0300

    Versão 1.0.0.2 Final

--- JS-Moedas-Cotação/api.js
+++ JS-Moedas-Cotação/api.js
@@ -1,33 +1,27 @@
+const CRIPTOS = ['bitcoin', 'ethereum', 'cardano', 'dogecoin', 'litecoin'];
+
 const API_CRIPTO = 'https://api.coingecko.com/api/v3/simple/price';
 
-const CRIPTOS = [
-  'bitcoin',
-  'ethereum',
-  'cardano',
-  'dogecoin',
-  'litecoin'
-];
-
 export async function buscarCriptos() {
-  const ids = CRIPTOS.join(',');
-  const url = `${API_CRIPTO}?ids=${ids}&vs_currencies=brl`;
+    const ids = CRIPTOS.join(',');
+    const url = `${API_CRIPTO}?ids=${ids}&vs_currencies=brl`;
 
-  const response = await fetch(url);
-  const dados = await response.json();
+    const response = await fetch(url);
+    const dados = await response.json();
 
-  return dados;
+    return dados;
 }
 
 
 const API_CAMBIO = 'https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,GBP-BRL';
 
 export async function buscarCambio() {
-  const response = await fetch(API_CAMBIO);
-  const dados = await response.json();
+    const response = await fetch(API_CAMBIO);
+    const dados = await response.json();
 
-  return {
-    usd: Number(dados.USDBRL.bid),
-    eur: Number(dados.EURBRL.bid),
-    gbp: Number(dados.GBPBRL.bid)
-  };
+    return {
+        usd: Number(dados.USDBRL.bid),
+        eur: Number(dados.EURBRL.bid),
+        gbp: Number(dados.GBPBRL.bid)
+    };
 }
